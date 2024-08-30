6cb52cf5f0d4b66f4094712e8d8a17829e21a896
rhystmorgan <morgan.rhystomas@gmail.com>
2024-08-30 13:22:23 +0000

    generate Owner credentials

--- ccf-loans-onchain/generate-credentials.ts
+++ ccf-loans-onchain/generate-credentials.ts
@@ -3,12 +3,12 @@ import { Lucid } from "https://deno.land/x/lucid@0.10.7/mod.ts";
 const lucid = await Lucid.new(undefined, "Preview");
 
 const privateKey = lucid.utils.generatePrivateKey();
-await Deno.writeTextFile("me.sk", privateKey);
+await Deno.writeTextFile("owner.sk", privateKey);
 
 const address = await lucid
   .selectWalletFromPrivateKey(privateKey)
   .wallet.address();
-await Deno.writeTextFile("me.addr", address);
+await Deno.writeTextFile("owner.addr", address);
 
 // run :
 // deno run --allow-net --allow-write generate-credentials.ts
